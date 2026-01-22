e79a02bc2a00d53b7b64424ae314a554b9237fbb
nilson-xavier <nilson77@gmx.com>
2026-01-22 14:30:52 -0300

    Testes de Repositorio

--- src/main/java/br/com/vr/miniautorizador/MiniautorizadorApplication.java
+++ src/main/java/br/com/vr/miniautorizador/MiniautorizadorApplication.java
@@ -3,7 +3,7 @@ package br.com.vr.miniautorizador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.vr.miniautorizador")
 public class MiniautorizadorApplication {
 
 	public static void main(String[] args) {
